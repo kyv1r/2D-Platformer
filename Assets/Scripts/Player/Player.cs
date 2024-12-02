924262e6f91e1dd8bdec1b1919f1a4af0ede38e9
kyv1r <kyv1.r@mail.ru>
2024-12-03 00:55:05 +1000

    Initial commit

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _jumpForce;
+
+    private Vector2 _moveDirection;
+    private PlayerInput _playerInput;
+    private Rigidbody2D _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+
+        _playerInput = new PlayerInput();
+
+        _playerInput.Player.Move.performed += OnMove;
+    }
+
+    private void FixedUpdate()
+    {
+        _moveDirection = _playerInput.Player.Move.ReadValue<Vector2>();
+        Move();
+    }
+
+    private void OnEnable()
+    {
+        _playerInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _playerInput.Disable();
+    }
+
+    private void Move()
+    {
+        Vector2 velocity = new Vector2(_moveDirection.x, _rigidbody.velocity.y);  // Сохраняем текущую скорость по Y (прыжок)
+        _rigidbody.velocity = velocity * _moveSpeed * Time.deltaTime;
+    }
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        _moveDirection = context.action.ReadValue<Vector2>();
+    }
+}
