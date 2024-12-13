98259d2984566ef9de4422c0cdad53eb403cd988
kyv1r <kyv1.r@mail.ru>
2024-12-14 03:01:02 +1000

    commit

---
+++ Assets/Scripts/UI/Health/HealthSmoothSlider.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class HealthSmoothSlider : MonoBehaviour
+{
+    [SerializeField] private HealthSystem _healthSystem;
+
+    private Slider _slider;
+    private Coroutine _coroutine;
+
+    private void Awake()
+    {
+        _slider = GetComponent<Slider>();
+
+        _slider.value = _healthSystem.CurrentValue;
+    }
+
+    private void OnEnable()
+    {
+        _healthSystem.HealthChanged += SetCurrentHealthValue;
+    }
+
+    private void OnDisable()
+    {
+        _healthSystem.HealthChanged -= SetCurrentHealthValue;
+    }
+
+    private void SetCurrentHealthValue(float healthValue)
+    {
+        _coroutine = StartCoroutine(SmoothChangeHealthValue(healthValue));
+    }
+
+    private IEnumerator SmoothChangeHealthValue(float healthValue)
+    {
+        while (_slider.value != healthValue)
+        {
+            _slider.value = Mathf.MoveTowards(_slider.value, healthValue, 0.09f);
+            yield return null;
+        }
+
+        if(_slider.value != healthValue)
+            yield break;
+    }
+}
